f35c6f835b99ae19ebdd11368b05d384c64b7047
Daniele Bueno <danni06@gmail.com>
2022-02-15 17:28:49 +0000

    exercicios bloco 05

---
+++ Fundamentos/bloco-5 -javascript-dom-eventos-e-web-storage/Revisao/elementos.js
@@ -0,0 +1,27 @@
+// arquivo script.js
+
+
+//Acesse o elemento elementoOndeVoceEsta .
+
+let elementoOndeVcEsta = document.getElementById("elementoOndeVoceEsta");
+
+/* Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele. */
+
+let pai = elementoOndeVcEsta.parentNode;
+pai.style.color = 'red';
+
+/* Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso? */
+
+let primeiroFilhodoFilho = elementoOndeVcEsta.firstElementChild;
+primeiroFilhodoFilho.innerText = "Estamos testando";
+
+
+/* 
+
+
+
+Acesse o primeiroFilho a partir de pai .
+Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+Agora acesse o terceiroFilho a partir de pai . */
