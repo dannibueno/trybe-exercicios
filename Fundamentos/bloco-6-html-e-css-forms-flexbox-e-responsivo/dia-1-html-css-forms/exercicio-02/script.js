e09f97c70428be1f06a55125aaf8e5804461e24a
Daniele Bueno <danni06@gmail.com>
2022-03-07 22:22:52 +0000

    Atualizando novamente  a branch bloco 6

--- Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio-02/script.js
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio-02/script.js
@@ -1,26 +1,26 @@
 const botaoSubmit = document.querySelector("#enviar");
 const botaoApagar = document.querySelector("#apagar");
 
-function submitForm(event){
+function submitForm(event) {
     event.preventDefault();
-console.log("teste");
+    console.log("teste");
 
 }
 
 botaoSubmit.addEventListener('click', submitForm);
 
 
-function apagaForm(){
+function apagaForm() {
 
-/*     const fullNameInput = document.querySelector("#full-name")
-    fullNameInput.value = ''; */
+    /*     const fullNameInput = document.querySelector("#full-name")
+        fullNameInput.value = ''; */
 
-    const todosInput = document.querySelectorAll ("input");
-    const textArea = document.querySelector ("textarea");
-    
+    const todosInput = document.querySelectorAll("input");
+    const textArea = document.querySelector("textarea");
+
     for (const inputdaVez of todosInput) {
-        inputdaVez.value= '';    
-        inputdaVez.checked = false;    
+        inputdaVez.value = '';
+        inputdaVez.checked = false;
     }
     textArea.value = '';
 }
@@ -34,16 +34,16 @@ function enableSubmit() {
     const submitBtn = document.querySelector('#enviar');
     const agreement = document.querySelector('#concordar');
     submitBtn.disabled = !agreement.checked;
-  }
+}
 
-  window.onload = function() {
+window.onload = function() {
     const clearBtn = document.querySelector('#apagar');
     clearBtn.addEventListener('click', apagaForm);
     const submitBtn = document.querySelector('#enviar');
     submitBtn.addEventListener('click', submitForm);
     const agreement = document.querySelector('#concordar');
     agreement.addEventListener('change', enableSubmit);
-  };
+};
 
 /*   Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela.
  */
@@ -53,20 +53,19 @@ function enableSubmit() {
 function textInputValidation() {
     const email = document.querySelector('#email').value.length;
     const invalidEmail = email < 10 || email > 50;
-  
+
     const name = document.querySelector('#full-name').value.length;
     const invalidName = name < 10 || name > 40;
-  
+
     const reason = document.querySelector('#resposta').value.length;
     const invalidReason = reason > 500;
-  
+
     if (invalidEmail || invalidName || invalidReason) {
-      return false;
+        return false;
+    } else {
+        return true;
     }
-    else {
-      return true;
-    }
-  }
+}
 
 
 
@@ -74,9 +73,8 @@ function submitForm(event) {
     event.preventDefault();
     const validation = textInputValidation();
     if (validation === false) {
-      alert('Dados inválidos');
+        alert('Dados inválidos');
     } else {
-      alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
     }
-  }
-
+}
