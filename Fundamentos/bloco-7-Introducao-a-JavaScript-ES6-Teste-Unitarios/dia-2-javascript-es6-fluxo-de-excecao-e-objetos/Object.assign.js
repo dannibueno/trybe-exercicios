904a28d85a9d71e876a3b2cff7b1a7cdfa00f454
Daniele Bueno <danni06@gmail.com>
2022-03-07 22:18:24 +0000

    Atualizando a branch bloco 6

---
+++ Fundamentos/bloco-7-Introducao-a-JavaScript-ES6-Teste-Unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/Object.assign.js
@@ -0,0 +1,34 @@
+const person = {
+    name: 'Alberto',
+    lastName: 'Gomes',
+    age: 20,
+  };
+  
+  const info = {
+    age: 23,
+    job: 'engenheiro',
+  };
+  
+  const family = {
+    children: ['Maria', 'João'],
+    wife: 'Ana',
+  };
+  
+  Object.assign(person, info, family)
+  console.log(person)
+
+
+  ////////////////////////////////////////////
+
+const person = {
+    name:'Roberto',
+  };
+  
+  const lastName = {
+    lastName: 'Silva',
+  };
+  
+  const newPerson = Object.assign({},person,lastName);
+  newPerson.name = 'Gilberto';
+  console.log(newPerson);
+  console.log(person);
